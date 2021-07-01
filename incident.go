332a8898e0b547535764a2090d145b2eb62ea699
sswensen <sswensen@summitdrift.com>
2021-07-01 14:00:17 -0600

    Fixes

--- incident.go
+++ incident.go
@@ -14,7 +14,7 @@ type Incident struct {
 	Name    string `json:"name"`
 	Message string `json:"message"`
 	Status  int    `json:"status"`
-	Visible int    `json"visible"`
+	Visible int    `json:"visible"`
 	Notify  bool   `json:"notify"`
 
 	ComponentID     int `json:"component_id"`
